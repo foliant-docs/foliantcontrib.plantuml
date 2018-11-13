5005f06e2b6856f72753750de7e03c13801a9e16
Artemy Lomov <lomov@restream.rt.ru>
2018-11-13 19:26:28 +0300

    Do not rewrite source Markdown file if an error occurs.

--- foliant/preprocessors/plantuml.py
+++ foliant/preprocessors/plantuml.py
@@ -186,7 +186,10 @@ class Preprocessor(BasePreprocessor):
             with open(markdown_file_path, encoding='utf8') as markdown_file:
                 content = markdown_file.read()
 
-            with open(markdown_file_path, 'w', encoding='utf8') as markdown_file:
-                markdown_file.write(self.process_plantuml(content))
+            processed_content = self.process_plantuml(content)
+
+            if processed_content:
+                with open(markdown_file_path, 'w', encoding='utf8') as markdown_file:
+                    markdown_file.write(processed_content)
 
         self.logger.info('Preprocessor applied')
